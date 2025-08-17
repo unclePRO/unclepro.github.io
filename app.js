81a291d6428d5f191b2d576544e235c3726dadba
unclePRO <90380822+unclePRO@users.noreply.github.com>
2025-08-18 00:09:43 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// PATCH: add listener to upgrade-now-btn at setupPremiumButton() bottom
-        const upgradeNow = document.getElementById('upgrade-now-btn');
-        if (upgradeNow) {
-            upgradeNow.addEventListener('click', (e) => {
-                e.preventDefault();
-                this.createFlameExplosion(upgradeNow);
-                this.showPremiumModal();
-            });
-        }
